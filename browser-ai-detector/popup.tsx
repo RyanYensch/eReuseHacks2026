73fc931995ee9af605c35520f912b459f5c41fef
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-03-13 23:04:18 +1100

    Feat: Sample first page for video demo and awaiting input

--- browser-ai-detector/popup.tsx
+++ browser-ai-detector/popup.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react"
+import React from 'react'
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="popup-window">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col py-2 pl-4 w-full bg-slate-400">
+          <h1 className='text-2xl font-bold'>AI Detector title...</h1>
+          <p className='text-sm'>Brought by eReuse Hacks (Unofficial)</p>
+
+        </div>
+
+        {/* Add a small horizontal line here to add break */}
+
+        {/* USAGE GUIDE */}
+
+        {/* Some gif / video to show a demo */}
+        <div className='w-full  flex flex-col items-center justify-center py-40 bg-blue-500'>
+          <h2 className='text-2xl font-bold'>Demo video</h2>
+          <p>Some screenrecorded demo video here</p>
+          <p>Right-click on a webpage and select the extension</p>
+        </div>
+
+        <p className='text-bold text-lg'>Awaiting input...</p>
+      </div>
     </div>
   )
 }
