d6b0e6aff6419f7debdcec211e177f8504ff6fc2
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-03-13 23:44:16 +1100

    Feat: Added placeholder youtube video

--- browser-ai-detector/popup.tsx
+++ browser-ai-detector/popup.tsx
@@ -6,24 +6,22 @@ function IndexPopup() {
   return (
     <div className="popup-window">
       <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col py-2 pl-4 w-full bg-slate-400">
-          <h1 className='text-2xl font-bold'>AI Detector title...</h1>
-          <p className='text-sm'>Brought by eReuse Hacks (Unofficial)</p>
-
+        <div className="flex flex-col py-2 pl-4 w-full bg-gray-800">
+          <h1 className='text-3xl'>AI Detector title...</h1>
+          <p className='text-lg'>Usage Guide</p>
         </div>
 
         {/* Add a small horizontal line here to add break */}
+        <div className="h-px w-full bg-slate-500" />
 
-        {/* USAGE GUIDE */}
 
         {/* Some gif / video to show a demo */}
-        <div className='w-full  flex flex-col items-center justify-center py-40 bg-blue-500'>
-          <h2 className='text-2xl font-bold'>Demo video</h2>
-          <p>Some screenrecorded demo video here</p>
-          <p>Right-click on a webpage and select the extension</p>
+        <div className='w-full flex flex-col items-center justify-center pt-20 gap-10'>
+          <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=aReM6MBodtFcRTC7" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+          <p className='text-sm'>Right-click on a webpage and select the extension</p>
         </div>
 
-        <p className='text-bold text-lg'>Awaiting input...</p>
+
       </div>
     </div>
   )
