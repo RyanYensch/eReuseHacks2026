bfdea0574958a88d4eb523ac1df5b96f1689ae9a
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-03-13 23:55:42 +1100

    Feat: Added simple instructions and footer

--- browser-ai-detector/popup.tsx
+++ browser-ai-detector/popup.tsx
@@ -6,22 +6,28 @@ function IndexPopup() {
   return (
     <div className="popup-window">
       <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col py-2 pl-4 w-full bg-gray-800">
+        {/* Header */}
+        <div className="flex flex-col py-2 px-4 w-full bg-gray-800">
           <h1 className='text-3xl'>AI Detector title...</h1>
           <p className='text-lg'>Usage Guide</p>
         </div>
 
-        {/* Add a small horizontal line here to add break */}
+        {/* Horizontal line */}
         <div className="h-px w-full bg-slate-500" />
 
-
         {/* Some gif / video to show a demo */}
-        <div className='w-full flex flex-col items-center justify-center pt-20 gap-10'>
+        <div className='w-full flex flex-col items-center justify-center py-20 gap-10'>
           <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=aReM6MBodtFcRTC7" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <p className='text-sm'>Right-click on a webpage and select the extension</p>
+          <p className='text-sm text-center px-4'>Right-click on a webpage, select the extension, highlight desired text and images</p>
         </div>
 
-
+        {/* Horizontal line */}
+        <div className="h-px w-full bg-slate-500" />
+        
+        {/* Footer */}
+        <div className="flex flex-row">
+          <p className='justify-start align-bottom'>Brought by eReuse Hacks (Unofficial)</p>
+        </div>
       </div>
     </div>
   )
