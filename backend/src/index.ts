e69a12d8f30f4e977bd9372f1cbaf4f68e855e8d
Ryan Yensch <ry2005@outlook.com>
2026-03-13 20:40:39 +1100

    Set up backend to run

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+
+import healthRouter from "./routes/health"
+import { error } from "node:console";
+
+const app = express();
+const port = Number(process.env.PORT || 6767);
+
+app.use(
+    cors({
+        origin: true
+    })
+);
+
+app.use(express.json({ limit: "1mb" }))
+
+app.get("/", (_req, res) => {
+    res.json({
+        ok: true,
+        message: "Backend is running"
+    });
+});
+
+app.use("/health", healthRouter);
+
+app.use((req, res) => {
+    res.status(404).json({
+        ok: false,
+        error: `Route not found: ${req.method} ${req.originalUrl}`
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Backend listening on http://localhost:${port}`);
+});
