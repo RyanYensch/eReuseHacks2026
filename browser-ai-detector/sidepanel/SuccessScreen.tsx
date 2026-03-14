7cf0faba8c757dbb099f0ce78809084ebddd611a
l-duong05 <176846912+l-duong05@users.noreply.github.com>
2026-03-14 21:26:27 +1100

    finished side panel result page (#38)
    * added base files for the different sidepanel screens
    
    * fixed screen spellings
    
    * added fake api calls and panel state updates in the background controller
    
    * finished linking setState listeners to panelState attr in session storage
    
    * fixed opening panel issue and finalised linking backend to frontend
    
    * removed unnecessary imports
    
    * also added props for success and error screens to use
    
    * added gauge component
    
    * added base design for results page
    
    * finished visuals for image and text

--- browser-ai-detector/sidepanel/SuccessScreen.tsx
+++ browser-ai-detector/sidepanel/SuccessScreen.tsx
@@ -1,19 +1,28 @@
 import GaugeComponent from "~node_modules/react-gauge-component/dist/lib";
+import "./success.css"
 
 export function SuccessScreen({ result }) {
   return (
     // This is where the results will be shown
-    <>
-      <h2>Results</h2>
-      <p>{result.likelihood}</p>
-      {/* 
-        Credits to this guy for giving a sandbox to custom gauge 
-        https://antoniolago.github.io/react-gauge-component/
-      */}
-      <GaugeComponent
-        value={parseFloat(result.percentage)}
-        type="semicircle"
-        arc={{
+    <div className="flex flex-col justify-center h-screen bg-gray-800">
+      <div className="success-page">
+        <h1 className="mb-4 text-3xl font-bold text-white text-heading md:text-5xl lg:text-6xl">
+          Browser&nbsp;
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            AI Detector
+          </span>
+        </h1>
+        <h2 className="text-lg font-medium text-white text-body lg:text-xl pb-4">
+          {result.likelihood}
+        </h2>
+        {/*
+          Credits to this guy for giving a sandbox to custom gauge
+          https://antoniolago.github.io/react-gauge-component/
+        */}
+        <GaugeComponent
+          value={parseFloat(result.percentage)}
+          type="semicircle"
+          arc={{
             gradient: true,
             width: 0.15,
             padding: 0,
@@ -49,17 +58,33 @@ export function SuccessScreen({ result }) {
                 { value: 60 },
                 { value: 80 }
               ],
-              defaultTickValueConfig: { style: { fontSize: "9px", fill: "#000000" } },
+              defaultTickValueConfig: { style: { fontSize: "9px", fill: "#ffffffcb" } },
               defaultTickLineConfig: {
                 distanceFromText: 8,
                 distanceFromArc: 5,
                 width: 1,
                 length: 4,
-                color: "#000000"
+                color: "#ffffffc2"
               }
             }
           }}
-      />
-    </>
+        />
+        {result.kind === "image" ? (
+          <>
+            <h2 className="mb-4 mt-6 text-xl font-bold text-white text-heading md:text-3xl lg:text-4xl">
+              Your Image
+            </h2>
+            <img className="w-screen h-auto px-4" src={result.imageUrl} />
+          </>
+        ) : (
+          <>
+            <h2 className="mb-2 mt-4 text-xl font-bold text-white text-heading md:text-3xl lg:text-4xl">
+              Your Text
+            </h2>
+            <p className="text-lg font-normal text-white text-body lg:text-xl pb-4">{result.preview}</p>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
