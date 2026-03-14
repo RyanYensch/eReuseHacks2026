51753ee7c60f955d17decef9d3fbb2071c5fbf17
Ryan Yensch <ry2005@outlook.com>
2026-03-14 21:05:15 +1100

    Finished the error page

--- browser-ai-detector/sidepanel/ErrorScreen.tsx
+++ browser-ai-detector/sidepanel/ErrorScreen.tsx
@@ -1,20 +1,22 @@
-import Typography from '@mui/material/Typography'
+// @ts-ignore
+import robotImage from "url:~assets/thinking-robot-error.png"
 
 export function ErrorScreen({ error }) {
   return (
-    <div className='flex items-center justify-center min-bs-[100dvh] relative p-6 overflow-x-hidden'>
+    <div className="flex flex-col justify-center h-screen bg-gray-800">
       <div className='flex items-center flex-col text-center gap-10'>
         <div className='flex flex-col gap-2 is-[90vw] sm:is-[unset]'>
-          <Typography variant='h4'>An Error Occurend</Typography>
-          <Typography className='font-medium text-8xl' color='text.primary'>
-            Please Try Again Later!
-          </Typography>
-          <Typography>{error}</Typography>
+          <h1 className="mb-4 text-6xl font-bold text-white text-heading md:text-7xl lg:text-8xl">
+            ERROR
+          </h1>
+          <p className="text-2xl font-medium text-white text-body lg:text-xl pb-2">Please Try Again Later!</p>
+          <p className="text-lg font-normal text-white text-body lg:text-xl pb-4">{error}</p>
         </div>
+        {/* https://www.streamlinehq.com/illustrations/bruxelles-duotone/technology?icon=ico_7bTFCA1Rl4deODdX */}
         <img
-          alt='Image'
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFz5fYgrepcP696Zm4ZtlldGLC6hEr83HUhDcUWJoyK2l-Rqp2YrkMskunDA_R5Hy-722xpQrOgG2-ZhAuOhuQ1aBRtwaf4jp3tfRNlNQKmg&s=10'
-          className='object-cover bs-[400px] md:bs-[450px] lg:bs-[500px]'
+          alt='Streamline Image'
+          src={robotImage}
+          className='object-cover bs-[400px] md:bs-[450px] lg:bs-[500px] w-50% h-50%'
         />
       </div>
     </div>
