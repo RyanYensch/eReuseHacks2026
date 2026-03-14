ac6cae40368da8d27eec849b1aca959c7371122d
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-03-14 21:40:42 +1100

    Feat: Replaced progress bar with spinning loading icon

--- browser-ai-detector/sidepanel/LoadingScreen.tsx
+++ browser-ai-detector/sidepanel/LoadingScreen.tsx
@@ -1,50 +1,22 @@
-import { useEffect, useState } from "react"
+import { RotatingLines } from 'react-loader-spinner';
 import "../style.css"
 
 export function LoadingScreen() {
-  const words = ['Inspecting elements...', 'Analysing data...', 'Evaluating score...', 'Finalising...'];
-  const [currentWord, setCurrentWord] = useState(0);
-  const [progress, setProgress] = useState(0)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((counter) => {
-        if (counter >= 100) {
-          // Stop at 100 for progress bar max width
-          clearInterval(interval);
-          return 100;
-        }
-        if (counter === 25 || counter === 50 || counter === 75) {
-          // Cycle to another word
-          setCurrentWord((prev) => prev + 1);
-        }
-        // Increase percentage by 1 after 67ms
-        return counter + 1;
-      });
-    }, 90);
-
-    return () => clearInterval(interval);
-  }, [])
-
+  const MyLoader = () => (
+    <RotatingLines
+      strokeColor="grey"
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="96"
+      visible={true}
+    />
+  );
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-800">
-      {/* Cycle through each word */}
-      <p className="text-lg text-white pb-3 tracking-widest uppercase">
-        {words[currentWord]}
-      </p>
-
-      {/* Progress Bar background */}
-      <div className="w-80 h-2 bg-gray-200 rounded-full overflow-hidden">
-        {/* Progress Bar fill */}
-        <div
-          className="h-full bg-green-600 rounded-full transition-all duration-100 ease-linear"
-          style={{ width: `${progress}%` }}
-        />
-      </div>
-
-      <p className="text-sm text-white pt-3">
-        {progress}%
+    <MyLoader />
+      <p className="text-lg text-white pt-3 tracking-widest uppercase">
+        Loading...
       </p>
     </div>
   )
